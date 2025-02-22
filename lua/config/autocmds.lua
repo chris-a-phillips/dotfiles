391b39f0bb1002c6fe4943454e5b128198257e01
Chris Phillips <phillipsachris@gmail.com>
2025-02-21 21:31:59 -0800

    Add autocommands for screenkey start and relative line numbers in explorer

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,23 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+--
+
+-- Automatically start screenkey
+local screenkey = require("screenkey")
+
+-- Ensure screenkey is initialized before using it
+screenkey.setup({})
+
+-- Auto-start screenkey when Neovim enters the UI
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    screenkey.toggle() -- Automatically turns it on
+  end,
+})
+
+-- Enable numbers and relative numbers in snacks explorer
+vim.defer_fn(function()
+  vim.cmd("set number")
+  vim.cmd("set relativenumber")
+end, 100) -- Small delay to ensure LazyVim finishes loading
